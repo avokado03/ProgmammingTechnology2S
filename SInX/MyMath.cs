c72c230928a39c92150d34a00a9c9126d8255d3f
Элина Граур <alotofcode95@gmail.com>
2020-04-17 23:33:15 +0300

    Add Sine task

---
+++ SInX/MyMath.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SInX
+{
+    public static class MyMath
+    {
+        public static double Factorial(int N)
+        {
+            if (N < 0)
+                return 0;
+            if (N == 0)
+                return 1;
+            else
+                return N * Factorial(N - 1);
+        }
+
+        public static double Sin(double x, double eps)
+        {
+            return GetMaclaurinSinCos(x, eps, 1);
+        }
+
+        public static double Cos(double x, double eps)
+        {
+            return GetMaclaurinSinCos(x, eps, 0);
+        }
+
+        /// <summary>
+        /// Recursive Method for calculating sine of an angle
+        /// </summary>
+        /// <param name="x">Angle (rad) </param>
+        /// <param name="eps">Аccuracy</param>
+        /// <param name="sum">Sum of all items in range</param>
+        /// <param name="n">Counter (0 -> infinity)</param>
+        /// <param name="sign">Sign of item (as Math.Pow(-1,n))-</param>
+        /// <param name="factOf">For getting factorial's param (as 2*n+1).</param>
+        /// <returns></returns>
+        private static double GetMaclaurinSinCos(double x, double eps, int factOf, double sum = 0, int n = 0, short sign = 1)
+        {
+            //We can make this operation faster
+            //double xn = Math.Pow(-1, n) * Math.Pow(x, 2*n + 1) / Factorial(2*n+1);
+            //by two cumulative parameters
+            //sign -> Math.Pow(-1, n)
+            //factOf -> (2*n+1)
+            double xn = sign * (Math.Pow(x, factOf) / Factorial(factOf));
+            if (Math.Abs(xn) > eps)
+            {
+                return GetMaclaurinSinCos(x, eps, factOf += 2, sum + xn,  ++n, sign*=-1);
+            }
+            else
+            {
+                return sum;
+            }
+        }
+    }
+}
---
+++ SInX/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SInX
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double x, eps;
+            Console.WriteLine("Введите величину угла в радианах: ");
+            try
+            {
+                
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите точность: ");
+                eps = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("MyMath.Sin({0}) = {1}",x, MyMath.Sin(x, eps));
+                Console.WriteLine("Math.Sin({0}) = {1}", x, Math.Sin(x));
+                Console.WriteLine("MyMath.Cos({0}) = {1}", x, MyMath.Cos(x, eps));
+                Console.WriteLine("Math.Cos({0}) = {1}", x, Math.Cos(x));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
